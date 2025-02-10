035a6b9a4f1dbdf76bcaf77490658b2dd5182ce4
HanineHassayoune <hassayounehanine@gmail.com>
2025-02-10 21:19:19 +0100

    commit 6

--- src/app/dashboards/dashboard-developpeur/tickets-dev/tickets-dev.component.ts
+++ src/app/dashboards/dashboard-developpeur/tickets-dev/tickets-dev.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TableComponent } from '../../../components/table/table.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tickets-dev',
   standalone: true,
-  imports: [TableComponent],
+  imports: [TableComponent,CommonModule],
   templateUrl: './tickets-dev.component.html',
   styleUrl: './tickets-dev.component.css'
 })
@@ -24,5 +25,22 @@ export class TicketsDevComponent {
     { ticketName: 'Feature XYZ', deadline: '2025-02-12', developer: 'Alice', status: 'In Progress', team: 'Back-End' },
     { ticketName: 'Hotfix #456', deadline: '2025-02-08', developer: 'Alice', status: 'Done', team: 'Back-End' }
   ];
+
+
+  isModalOpen = false;
+
+  openModal() {
+    this.isModalOpen = true;
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+  }
+
+  submitSolution() {
+    alert('Solution est ajoutée avec succès !');
+    this.closeModal();
+  }
+
 
 }
